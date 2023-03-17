8b245a76b4d3594533493295f075dfeebf17d47b
Kyle Jussab <kylejussab@gmail.com>
2023-03-16 22:21:03 -0700

    Added playable ITP1 game

---
+++ scripts/itp1-game.js
@@ -0,0 +1,22 @@
+window.addEventListener('keydown', function(e) {
+    if(e.keyCode == 32 && e.target == document.body) {
+      e.preventDefault();
+    }
+
+    if(e.keyCode == 37 && e.target == document.body) {
+      e.preventDefault();
+    }
+
+    if(e.keyCode == 38 && e.target == document.body) {
+      e.preventDefault();
+    }
+
+    if(e.keyCode == 39 && e.target == document.body) {
+      e.preventDefault();
+    }
+
+    if(e.keyCode == 40 && e.target == document.body) {
+        e.preventDefault();
+    }
+  });
+

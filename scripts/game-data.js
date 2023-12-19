296d468b8d8860e1ec09d9ce9760dcfa3dbb68ed
Kyle Jussab <kylejussab@gmail.com>
2023-12-18 18:48:03 -0800

    Updated all game cards

--- scripts/game-data.js
+++ scripts/game-data.js
@@ -6,7 +6,7 @@ let gameData = [
         "alt": "C Sharp code",
         "title": "The Architect Hestia",
         "subtitle": "Third Person Shooter / Survival / Adventure - Coming soon",
-        "description": "Elian, a Rusthavener, is tired of the opression forced on them by Auravale. Little does he know, with each death he switches sides.\n\nContributions:\n- Implemented player movement mechanics and animations.\n- Engineered dynamic camera interactions.\n- Engineered dynamic camera interactions.\n- Created AI systems with idle, pursue, and attack states.\n- Programmed limb-specific damage mechanics",
+        "description": "Elian, a Rusthavener, is tired of the opression forced on him by Auravale. Little does he know, with each death he switches sides.\n\nContributions:\n- Implemented player movement mechanics and animations.\n- Engineered dynamic camera interactions.\n- Engineered dynamic camera interactions.\n- Created AI systems with idle, pursue, and attack states.\n- Programmed limb-specific damage mechanics",
         "link": "thearchitecthestia.html"
     },
     {
@@ -16,7 +16,7 @@ let gameData = [
         "alt": "C Sharp code",
         "title": "Project Phoebe",
         "subtitle": "Third Person Platformer / Puzzler - April 2023",
-        "description": "Master your skills getting through rooms by competing in 5 different modes, all of which challenge you in a different and unique way.",
+        "description": "Master your skills getting through rooms by competing in 5 different modes, all of which challenge you in a different and unique way.\n\nContributions:\n- Sole developer overseeing level design, game design, and coding.\n- Implemented a puzzle system with sequence memory.\n- Developed AI capable of targeting objects.\n- Built a game state system enabling checkpoints.\n- Designed the main and pause menus.",
         "link": "projectphoebe.html"
     },
     {
@@ -26,7 +26,7 @@ let gameData = [
         "alt": "2d platformer showing character facing right, looking at collectable",
         "title": "ITP1 Project",
         "subtitle": "2D Platformer - September 2021",
-        "description": "The game project I submitted for University of London in their Introduction to Programming 1 module.",
+        "description": "The game project I submitted for University of London in their Introduction to Programming 1 module.\n\nContributions:\n- Sole developer responsible for coding, game and level design.\n- Developed custom gravity mechanics using velocity and acceleration.\n- Created the artwork for the main character, enemies, pickups, and UI.\n- Implemented a power up system.",
         "link": "itp1project.html"
     }
 ]

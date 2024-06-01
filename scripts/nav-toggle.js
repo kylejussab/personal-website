30994f0d1310be4074b0f31658accd4087ef98b1
Kyle Jussab <kylejussab@gmail.com>
2024-06-01 03:26:01 -0700

    Revamp

--- scripts/nav-toggle.js
+++ scripts/nav-toggle.js
@@ -2,31 +2,33 @@ let gameElements = document.getElementsByClassName('games-content');
 let storyElements = document.getElementsByClassName('stories-content');
 let projectElements = document.getElementsByClassName('projects-content');
 
+let aboutContent = document.getElementsByClassName('about-content');
+
 function selected(){
     if(document.getElementById('games').checked){
         for(let i = 0; i < gameElements.length; i++){
             gameElements[i].style.display = 'flex';
         }
 
-        for(let i = 0; i < storyElements.length; i++){
-            storyElements[i].style.display = 'none';
-        }
-
         for(let i = 0; i < projectElements.length; i++){
             projectElements[i].style.display = 'none';
         }
+
+        for(let i = 0; i < aboutContent.length; i++){
+            aboutContent[i].style.display = 'none';
+        }
     }
-    else if(document.getElementById('stories').checked){
+    else if(document.getElementById('about').checked){
         for(let i = 0; i < gameElements.length; i++){
             gameElements[i].style.display = 'none';
         }
 
-        for(let i = 0; i < storyElements.length; i++){
-            storyElements[i].style.display = 'flex';
-        }
-
         for(let i = 0; i < projectElements.length; i++){
             projectElements[i].style.display = 'none';
+        }
+
+        for(let i = 0; i < aboutContent.length; i++){
+            aboutContent[i].style.display = 'flex';
         }
     }
     else if(document.getElementById('projects').checked){
@@ -34,12 +36,12 @@ function selected(){
             gameElements[i].style.display = 'none';
         }
 
-        for(let i = 0; i < storyElements.length; i++){
-            storyElements[i].style.display = 'none';
-        }
-
         for(let i = 0; i < projectElements.length; i++){
             projectElements[i].style.display = 'flex';
+        }
+
+        for(let i = 0; i < aboutContent.length; i++){
+            aboutContent[i].style.display = 'none';
         }
     }
 }

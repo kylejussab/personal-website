04044c64c6ed3324b0a4dc1a004c6c202d84ec3b
Kyle Jussab <kylejussab@gmail.com>
2024-06-04 22:54:49 -0700

    Cards and cleanup

--- scripts/game-data.js
+++ scripts/game-data.js
@@ -1,33 +1,29 @@
 let gameData = [
     {
-        "icon": "assets/unity.png",
-        "icon2": "assets/c-sharp.png",
+        "techStack": "Unity, C#",
         "image": "assets/The Architect Hestia.png",
         "alt": "C Sharp code",
         "title": "The Architect Hestia",
         "subtitle": "Third Person Shooter / Survival / Adventure - Coming soon",
-        "description": "Elian, a Rusthavener, is tired of the opression forced on him by Auravale. Little does he know, with each death he switches sides.\n\nContributions:\n- Implemented player movement mechanics and animations.\n- Engineered dynamic camera interactions.\n- Created AI systems with idle, pursue, and attack states.\n- Programmed limb-specific damage mechanics.",
-        "link": "https://kylejussab.com/thearchitecthestia",
-        "recommend": "yes"
+        "description": "Elian, a Rusthavener, is tired of the opression forced on him by Auravale. Little does he know, with each death he switches sides.",
+        "link": "https://kylejussab.com/thearchitecthestia"
     },
     {
-        "icon": "assets/unity.png",
-        "icon2": "assets/c-sharp.png",
+        "techStack": "Unity, C#",
         "image": "assets/Project Phoebe.png",
         "alt": "C Sharp code",
         "title": "Project Phoebe",
         "subtitle": "Third Person Platformer / Puzzler - April 2023",
-        "description": "Master your skills getting through rooms by competing in 5 different modes, all of which challenge you in a different and unique way.\n\nContributions:\n- Sole developer overseeing level design, game design, and coding.\n- Implemented a puzzle system with sequence memory.\n- Developed AI capable of targeting objects.\n- Built a game state system enabling checkpoints.\n- Designed the main and pause menus.",
+        "description": "Master your skills getting through rooms by competing in 5 different modes, all of which challenge you in a different and unique way.",
         "link": "https://kylejussab.com/projectphoebe"
     },
     {
-        "icon": "assets/p5js.png",
-        "icon2": "assets/js.png",
+        "techStack": "p5.js, JavaScript",
         "image": "assets/itp1project.png",
         "alt": "2d platformer showing character facing right, looking at collectable",
         "title": "ITP1 Project",
         "subtitle": "2D Platformer - September 2021",
-        "description": "The game project I submitted for University of London in their Introduction to Programming 1 module.\n\nContributions:\n- Sole developer responsible for coding, game and level design.\n- Developed custom gravity mechanics using velocity and acceleration.\n- Created the artwork for the main character, enemies, pickups, and UI.\n- Implemented a power up system.",
+        "description": "The game project I submitted for University of London in their Introduction to Programming 1 module.",
         "link": "https://kylejussab.com/itp1project"
     }
 ]
@@ -36,36 +32,19 @@ const gameCardTemplate = document.querySelector("[data-game-template]");
 const gameCardContainer = document.querySelector("[data-game-cards-container]");
 
 posts = gameData.map(post => {
-    const card = gameCardTemplate.content.cloneNode(true).children[0]
-    
-    const recommendFront = card.querySelector("[data-recommend-front]")
-    const recommendBack = card.querySelector("[data-recommend-back]")
+    const card = gameCardTemplate.content.cloneNode(true).children[0];
 
-    if(post.recommend == "yes"){
-        recommendFront.innerHTML = '<span class="ribbon-front"><p class="ribbon-text">Kyle&apos;s recommendation</p></span>';
-        recommendBack.innerHTML = '<span class="ribbon-back"></span>';
-    }
+    const title = card.querySelector("[data-title]");
+    const subtitle = card.querySelector("[data-subtitle]");
+    const description = card.querySelector("[data-description]");
+    const image = card.querySelector("[data-img]");
+    const link = card.querySelector("[data-link]");
 
-    const title = card.querySelector("[data-title]")
-    const subtitle = card.querySelector("[data-subtitle]")
-    const description = card.querySelector("[data-description]")
-    const img = card.querySelector("[data-img]")
-    const link = card.querySelector("[data-link]")
-    const icon = card.querySelector("[data-icon]")
-    const icon2 = card.querySelector("[data-icon2]")
-
-    const image = '<img src="' + post.image + '" alt="' + post.alt + '">';
-    const iconImage = '<img src="' + post.icon + '" class="card-icon">';
-    const icon2Image = '<img src="' + post.icon2 + '" class="card-icon">';
-    const linkElement = '<div class="card-button-container"><a href="' + post.link + '" class="card-button"><p>See more</p><img src="assets/right-arrow.png"></a></div>';
-
-    title.textContent = post.title
-    subtitle.textContent = post.subtitle
-    description.innerHTML = post.description.replace(/\n/g, '<br>');
-    img.innerHTML = image;
-    icon.innerHTML = iconImage;
-    icon2.innerHTML = icon2Image;
-    link.innerHTML = linkElement;
+    title.innerHTML = post.title + ' <span>' + post.techStack + '</span>';
+    subtitle.textContent = post.subtitle;
+    description.textContent = post.description;
+    image.innerHTML = '<img src="' + post.image + '" alt="' + post.alt + '">';
+    link.href = post.link;
 
     gameCardContainer.append(card);
-})
+});
--- scripts/project-data.js
+++ scripts/project-data.js
@@ -1,44 +1,38 @@
 let projectData = [
     {
-        "icon": "assets/html.png",
-        "icon2": "assets/css.png",
+        "techStack": "HTML, CSS",
         "image": "assets/PixelPulse.png",
         "alt": "The homepage of the PixelPulse application",
         "title": "PixelPulse",
         "subtitle": "View and Share Coding Sketches - September 2023",
-        "description": "A 'social media' platform for UoL Computer Science students to share their coding projects. Used for inspiration and revision. Built in a team of 4 using Agile methodologies.\n\nContributions:\n- Crafted an appealing and user-friendly interface achieving an 82.5 SUS score.\n- Created an interactive prototype for iterative and user-centered design.\n- Conducted market research, analyzing trends and user expectations.",
-        "link": "https://kylejussab.com/pixelpulse",
-        "recommend": "yes"
+        "description": "A 'social media' platform for UoL Computer Science students to share their coding projects. Used for inspiration and revision. Built in a team of 4 using Agile methodologies.",
+        "link": "https://kylejussab.com/pixelpulse"
     },
     {
-        "icon": "assets/html.png",
-        "icon2": "assets/css.png",
+        "techStack": "HTML, CSS, SQLite3",
         "image": "assets/Microblogger.png",
         "alt": "The homepage of my microblogger application",
         "title": "Microblogger",
         "subtitle": "A Gaming Microblog - September 2023",
-        "description": "A dynamic website for my gaming blogs. Used SQLite3 to store and retrieve relevant information. Improved the styling with vanilla CSS.\n\nContributions:\n- Crafted front-end components using HTML/CSS focusing on layout, styling, and responsiveness.\n- Designed and implemented the back-end with SQLite3, handling database schema and user authentication.\n- Integrated interactive features like post liking and comments.",
+        "description": "A dynamic website for my gaming blogs. Used SQLite3 to store and retrieve relevant information. Improved the styling with vanilla CSS.",
         "link": "https://kylejussab.com/microblogger"
     },
     {
-        "icon": "assets/c++.png",
-        "icon2": "assets/apple.png",
+        "techStack": "JUCE, C++",
         "image": "assets/DJ Application.png",
         "alt": "The GUI of my DJ application",
         "title": "DJ Application",
         "subtitle": "Built with the JUCE Framework - March 2023",
-        "description": "Utilized OOP techniques throughout. Improved the application by adding data persistence and spinning records.\n\nContributions:\n- Utilized C++ to build core logic, incorporating audio manipulation tools like high/low pass filters and playback controls.\n- Implemented data persistence for efficient song library management.\n- Conducted extensive testing to ensure stability and functionality, rigorously examining features and performance.",
-        "link": "https://kylejussab.com/djapplication",
-        "recommend": "yes"
+        "description": "Utilized OOP techniques throughout. Improved the application by adding data persistence and spinning records.",
+        "link": "https://kylejussab.com/djapplication"
     },
     {
-        "icon": "assets/p5js.png",
-        "icon2": "assets/js.png",
+        "techStack": "p5.js, JavaScript",
         "image": "assets/Music Visualizer.png",
         "alt": "A visualizer from the music visualizer application",
         "title": "Music Visualizer",
         "subtitle": "Built with the p5.js Framework - September 2022",
-        "description": "Created a range of creative audio visualizations, including one that utilizes the built in camera. All visualizations react to the music being played.\n\nContributions:\n- Integrated audio features enabling track looping, scrubbing, and selection for enhanced user engagement.\n- Empowered users with customization options for visuals.\n- Strategically planned project timelines via Gantt Chart and conducted thorough usability testing with end users.",
+        "description": "Created a range of creative audio visualizations, including one that utilizes the built in camera. All visualizations react to the music being played.",
         "link": "https://kylejussab.com/musicvisualizer"
     }
 ]
@@ -47,37 +41,19 @@ const projectCardTemplate = document.querySelector("[data-project-template]");
 const projectCardContainer = document.querySelector("[data-project-cards-container]");
 
 posts = projectData.map(post => {
-    const card = projectCardTemplate.content.cloneNode(true).children[0]
+    const card = projectCardTemplate.content.cloneNode(true).children[0];
 
-    const recommendFront = card.querySelector("[data-recommend-front]")
-    const recommendBack = card.querySelector("[data-recommend-back]")
+    const title = card.querySelector("[data-title]");
+    const subtitle = card.querySelector("[data-subtitle]");
+    const description = card.querySelector("[data-description]");
+    const image = card.querySelector("[data-img]");
+    const link = card.querySelector("[data-link]");
 
-    if(post.recommend == "yes"){
-        recommendFront.innerHTML = '<span class="ribbon-front"><p class="ribbon-text">Kyle&apos;s recommendation</p></span>';
-        recommendBack.innerHTML = '<span class="ribbon-back"></span>';
-    }
-
-    const title = card.querySelector("[data-title]")
-    const subtitle = card.querySelector("[data-subtitle]")
-    const description = card.querySelector("[data-description]")
-    const img = card.querySelector("[data-img]")
-    const link = card.querySelector("[data-link]")
-    const icon = card.querySelector("[data-icon]")
-    const icon2 = card.querySelector("[data-icon2]")
-
-    const image = '<img src="' + post.image + '" alt="' + post.alt + '">';
-    const iconImage = '<img src="' + post.icon + '" class="card-icon">';
-    const icon2Image = '<img src="' + post.icon2 + '" class="card-icon">';
-    const linkElement = '<div class="card-button-container"><a href="' + post.link + '" class="card-button"><p>See more</p><img src="assets/right-arrow.png"></a></div>';
-
-    title.textContent = post.title
-    subtitle.textContent = post.subtitle
-    description.innerHTML = post.description.replace(/\n/g, '<br>');
-    img.innerHTML = image;
-    icon.innerHTML = iconImage;
-    icon2.innerHTML = icon2Image;
-    link.innerHTML = linkElement;
+    title.innerHTML = post.title + ' <span>' + post.techStack + '</span>';
+    subtitle.textContent = post.subtitle;
+    description.textContent = post.description;
+    image.innerHTML = '<img src="' + post.image + '" alt="' + post.alt + '">';
+    link.href = post.link;
 
     projectCardContainer.append(card);
-})
-
+});
